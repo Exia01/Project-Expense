4cd5e0b629773436fdeda0475b9f4cd8c5939b29
Exia01 <gonzalezj@protonmail.com>
2019-05-25 18:18:35 +0000

    setting up routes

--- server/app.js
+++ server/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 // const favicon = require('express-favicon');
-const app = express();
+// const mongooseConnector = require('./config/mongoose.js')
+const reportRoutes = require('./config/reportRoutes.js');
 const PORT = 5000;
+const multer = require('multer');
 
+const app = express();
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// views and static files
+app.set('views', path.join(__dirname, '/client/public/views'));
+app.use(express.static(path.join(__dirname, '/client/public/assets/css')));
+app.use(express.static(path.join(__dirname, '/client/public/assets/js')));
 
 //Mongoose connection --> need import from config
 // mongooseConnector.mongooseConnection().then((res) => {
@@ -15,7 +25,8 @@ const PORT = 5000;
 //   console.log('ERROR', err.message);
 // });
 
+app.use('/', reportRoutes);
 
-app.listen(PORT, function(){
+app.listen(PORT, function() {
 	console.log(`Server Listening on port ${PORT}`);
 });
