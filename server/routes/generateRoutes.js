f811645422ad405a7237f0a4b0196cf72f4b54f3
ehoversten <ehoversten@gmail.com>
2019-06-30 13:58:15 -0400

    added basic page layout with table for data read testing

--- server/routes/generateRoutes.js
+++ server/routes/generateRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log("Hit Read Route");
+    // console.log("Hit Read Route");
     // res.send("Hello There");
     res.render('../client/public/views/generate/index');
     // res.render('/generate/index')
