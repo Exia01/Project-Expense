94b881e568200bf99c72263161067e54bb5522c8
ehoversten <ehoversten@gmail.com>
2019-07-02 09:37:30 -0400

    played with reading a simple CSV txt file

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const generateRoutes		= require('./server/routes/generateRoutes')
 const accountRoutes 		= require('./server/routes/accountRoutes');
 const multer 				= require('multer');
 const passport          	= require('passport');
+const fs 					= require('fs');
 const PORT 					= process.env.PORT || 8000;
 
 const app = express();
@@ -33,7 +34,42 @@ app.use(passport.session());
 //Express body parser
 app.use(express.urlencoded({extended: true}));
 
+app.get('/', (req, res) => {
+	// This block of code will read from the "example.csv" file.
+	// It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+	// The code will store the contents of the reading inside the variable "data"
 
+	// Read our test file into the buffer
+	fs.readFile('things.txt', 'utf8', function (err, data) {
+	// If there is an error, Log it
+	if (err) {
+		console.log('Read Failed')
+		return console.log(err)
+	}
+	// Let's see the data
+	console.log('Data: ' + data)
+	// console.log("---------------");
+	// console.log(data);
+
+	// Then split it by commas (to make it more readable)
+	var dataArr = data.split(',')
+
+	// We will then re-display the content as an array for later use.
+	console.log("Data Array: " + dataArr)
+	});
+
+	// Break the string down by comma separation and store the contents into the output array.
+	var output = data.split(',')
+
+	// Loop Through the newly created output array
+	for (var i = 0; i < output.length; i++) {
+		// Print each element (item) of the array/
+		console.log(output[i]);
+	}
+
+	res.send('Root Route');
+
+});
 
 //Multer Storage
 // ('./server/utils/multerStorage.js')
@@ -45,7 +81,7 @@ app.use(express.urlencoded({extended: true}));
 //   console.log('ERROR', err.message);
 // });
 
-app.use('/', reportRoutes);
+app.use('/report', reportRoutes);
 app.use('/accounts', accountRoutes);
 app.use('/read', generateRoutes);
 
--- server/routes/generateRoutes.js
+++ server/routes/generateRoutes.js
@@ -1,13 +1,58 @@
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
+const fs      = require('fs');
 
 router.get('/', (req, res) => {
-    // console.log("Hit Read Route");
-    // res.send("Hello There");
+
+    // This block of code will read from the "example.csv" file.
+    // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+    // The code will store the contents of the reading inside the variable "data"
+
+    // Read our test file into the buffer
+    // **** ERROR reading the path of the file in ERROR ***** //
+    fs.readFile('things.txt', 'utf8', function(err, data) {
+        // If there is an error, Log it 
+        if(err) {
+            console.log("Read Failed");
+            return console.log(err);
+        }
+        // Let's see the data
+        console.log("Data: " + data);
+        console.log("---------------");
+        console.log(data);
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(',');
+
+        // We will then re-display the content as an array for later use.
+        console.log("Data Array: " + dataArr);
+        console.log('---------------');
+
+        console.log(dataArr);
+        console.log('---------------');
+
+
+        // Break the string down by comma separation and store the contents into the output array.
+        var output = data.split(',')
+
+        // Loop Through the newly created output array
+        for (var i = 0; i < output.length; i++) {
+            // Print each element (item) of the array/
+            console.log(output[i])
+        }
+
+
+    });
+
+
     res.render('../client/public/views/generate/index');
     // res.render('/generate/index')
 
 });
+
+
+
+
 
 
 module.exports = router;
