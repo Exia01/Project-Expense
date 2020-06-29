bb0ea4122100bac28ec18f63dbede680ebc23323
Jose <Gonzalezj@protonmail.com>
2020-06-28 20:49:15 -0400

    Update model to include proper required message

--- server/models/file.model.js
+++ server/models/file.model.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema({
-    name: { type: String, required: true },
-    isActive:{type:Boolean, required:true},
-    date: { type: Date, required: true },
+    name: { type: String, required: [true, 'File name is required'] },
+    originalName: { type: String, required: [true, 'Original file name is required'] },
+    fileLocation: { type: String, required: [true, 'File location is required'] },
+    size: { type: Number, },
+    mimeType: { type: String },
+    date: { type: Date, },
+    isActive: { type: Boolean, required: [true,'File status is Required'] },
+
 }, {
     timestamps: true,
 });
@@ -13,3 +18,9 @@ const fileSchema = new Schema({
 const Exercise = mongoose.model('File', fileSchema);
 
 module.exports = Exercise;
+
+
+
+// Embedding vs referencing:https://stackoverflow.com/questions/5373198/mongodb-relationships-embed-or-reference
+
+//Vide from MongoDB:https://www.youtube.com/watch?v=leNCfU5SYR8&t=0s
