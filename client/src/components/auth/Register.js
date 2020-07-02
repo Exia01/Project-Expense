790bb7ec1885fbd585d3d46554a0e2ea28dddeb8
ehoversten <ehoversten@gmail.com>
2020-07-02 15:40:37 -0400

    able to submit data from React Register component form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -12,33 +12,89 @@ const Register = () => {
         confirm: ''
     });
 
-    return (
-        <Fragment>
-            <h1 className="welcome">Welcome, Register!</h1>
-            <form className="register" action="/api/users">
-                <div className="form-group">
-                    <input type="text" placeholder="First Name" name="first"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" placeholder="Last Name" name="last"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" placeholder="Username" name="username"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" placeholder="Email" name="email"/>
-                </div>
-                <div className="form-group">
-                    <input type="password" placeholder="Password" name="password"/>
-                </div>
-                <div className="form-group">
-                    <input type="password" placeholder="Confirm Password" name="confirm"/>
-                </div>
-                <button type="submit" className="btn">Submit</button>
-            </form>
+    const { first, last, username, email, password, confirm } = formData;
 
-        </Fragment>
-    )
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value});
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if(password !== confirm) {
+            console.error("Passwords do not Match")
+        }
+
+        //-- TESTING --//
+        console.log("Submitting");
+        console.log(formData);
+    }
+
+
+    return (
+      <Fragment>
+        <h1 className="welcome">Welcome, Register!</h1>
+        <form className="register" onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="First Name"
+              name="first"
+              value={first}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Last Name"
+              name="last"
+              value={last}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="confirm"
+              value={confirm}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </Fragment>
+    );
 }
 
 export default Register;
