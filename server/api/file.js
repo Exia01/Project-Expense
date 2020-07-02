b5f7e70ead7af8ec91304519477a503064988f19
Jose <Gonzalezj@protonmail.com>
2020-07-02 16:09:00 -0400

    Multiple changes
    Refactor routes and reorganized code, added download. Update findByid, and delete routes

--- server/api/file.js
+++ server/api/file.js
@@ -6,72 +6,25 @@ const multer = require('multer')
 const multerModule = require('../utils/multerStorage.js');
 const fileService = require('../services/file');
 
-const getAllFiles = async (req, res, next) => {
+
+const getAllFiles = (req, res) => {
     try {
-        const data = File.find();
-        if (!fileData) {
-            const err = new Error('No files found');
-            err.status = 404;
-            throw err;
-        }
-        res.json(data);
+        fileService.file_index().then(foundFiles => {
+            return res.status(200).json(
+                { status: "true", message: 'Files found', files: foundFiles }
+            )
+        })
     } catch (e) {
-        next(e);
+        return res.status(404).json({ err: err.message })
     }
 };
 
-// router.route('/').get(getAllFiles);
 
+//Index
+router.route('/').get(getAllFiles);
 
-//Download
-router.route('/:id').delete((req, res, next) => {
-    fileService.file_find(req).then(foundFileObj => fileService.file_delete(foundFileObj.id))
-        .then(deletedFoundObj => {
-            try {
-                fs.unlinkSync(`${deletedFoundObj.fileLocation}`)
-                return res.status(200).json(
-                    { status: "true", message: 'FileDeleted', deleted: deletedFoundObj }
-                )
-                //file removed
-            } catch (err) {
-                console.error("Embedded err", err.message)
-                return res.status(500).json({ status: "false", message: 'Something went sideways', deletedFoundObj: deletedFoundObj })
-            }
-
-        }).catch(err => {
-            console.log("Catch all err, ", err.message);
-            return res.status(404).json({ err: err.message })
-        })
-
-})
-
-
-
-//Delete
-router.route('/:id').delete((req, res, next) => {
-    fileService.file_find(req).then(foundFileObj => fileService.file_delete(foundFileObj.id))
-        .then(deletedFoundObj => {
-            try {
-                fs.unlinkSync(`${deletedFoundObj.fileLocation}`)
-                return res.status(200).json(
-                    { status: "true", message: 'FileDeleted', deleted: deletedFoundObj }
-                )
-                //file removed
-            } catch (err) {
-                console.error("Embedded err", err.message)
-                return res.status(500).json({ status: "false", message: 'Something went sideways', deletedFoundObj: deletedFoundObj })
-            }
-
-        }).catch(err => {
-            console.log("Catch all err, ", err.message);
-            return res.status(404).json({ err: err.message })
-        })
-
-})
-
-
-//Upload
-router.post('/', function (req, res) {
+//Create
+router.post('/', function (req, res, next) {
     multerModule.upload(req, res, function (err) {
         // could also combine this try catch with finally and either send err or message
         if (err instanceof multer.MulterError) {
@@ -123,99 +76,78 @@ router.post('/', function (req, res) {
     })
 })
 
-// //SIMPLE IMPLEMENTATION
-// router.post('/upload', multerModule.upload.single('testfile'), (req, res, next) => {
-//     console.log(req.file);
-//     const tempFile = req.file
-//     if (!tempFile) {
-//         return res.status(400).json({
-//             status: false,
-//             data: 'No file provided.'
-//         });
-//     }
-//     return res.json({
-//         status: true,
-//         message: 'File is uploaded.',
-//         data: {
-//             name: tempFile.originalname,
-//             mimetype: tempFile.mimetype,
-//             size: tempFile.size
-//         }
-//     });
-// })
+//Find by ID
+router.route('/:id').get((req, res, next) => {
+    fileService.file_find(req.params.id)
+        .then(foundFileObj => {
+            if (!foundFileObj) {
+                throw new Error('Not Found')
+            }
+            return res.status(200).json(
+                { status: "true", message: 'File Found', file: foundFileObj }
+            )
+        }).catch(err => {
+            console.log("Catch all err, ", err);
+            return res.status(404).json({ status: false, err: err.message })
+        })
+
+})
+
+//Delete
+router.route('/:id').delete((req, res, next) => {
+    fileService.file_delete(req.id)
+        .then(deletedFoundObj => {
+            try {
+                fs.unlinkSync(`${deletedFoundObj.fileLocation}`)
+                return res.status(200).json(
+                    { status: "true", message: 'FileDeleted', deleted: deletedFoundObj }
+                )
+                //file removed
+            } catch (err) {
+                let message = "Something went sideways"
+                console.error("Embedded err", err.message)
+                if (!deletedFoundObj) {
+                    message = 'File not found or it has already been deleted' //at some point we could implement at soft delete option
+                }
+                return res.status(500).json({ status: "false", message, deletedFoundObj: deletedFoundObj })
+            }
+
+        }).catch(err => {
+            console.log("Catch all err, ", err.message);
+            return res.status(404).json({ err: err.message })
+        })
+
+})
+
+//Download
+router.route('/:id/download').get((req, res, next) => {
+    fileService.file_find(req.params.id)
+        .then(foundFileObj => {
+            if (!foundFileObj) {
+                throw new Error('Not Found')
+            }
+
+            return res.download(foundFileObj.fileLocation, foundFileObj.originalName, function (err) {
+                if (err) {
+                    // Handle error, but keep in mind the response may be partially-sent
+                    // so check res.headersSent
+                    console.log(err);
+                    throw new Error('Something went sideways')
+
+                } else {
+                    // decrement a download credit, etc.
+                    console.log("Success!");
+
+                }
+            })
 
 
-// // PREVIOUS VERSION
-// router.post('/upload', multerModule.upload.single('testfile'), async (req, res, next) => {
-//     const tempFile = req.file;
-//     if (!tempFile) {
-//         return res.status(400).json({
-//             status: false,
-//             data: 'No file provided.'
-//         });
-//     }
-//     try {
-//         let newFileDoc = {
-//             name: tempFile.filename,
-//             originalName: tempFile.originalname,
-//             fileLocation: tempFile.path,
-//             size: tempFile.size,
-//             mimeType: tempFile.mimetype,
-//             date: Date.now(),
-//             isActive: true,
-//         }
-//         fileService.file_new(req, res, newFileDoc, next).then(newFile => {
-//             console.log("Inside fileService", tempFile);
-//             res.json({
-//                 status: true,
-//                 message: 'File Uploaded',
-//                 data: {
-//                     name: tempFile.originalname,
-//                     mimetype: tempFile.mimetype,
-//                     size: tempFile.size
-//                 }
-//             })
-//         }).catch(err => {
-//             log("embedded catch error")
-//             res.status(403).json({ err })
-//         })
-//     } catch (err) {
-//         if (err instanceof multer.MulterError) {
-//             console.log("MULTER ERROR");
-//         }
-//         console.log("Error!!!!", err);
-//         res.status(403).json({ err });
-//     }
-// });
+        }).catch(err => {
+            console.log("Catch all err, ", err);
+            return res.status(404).json({ status: false, err: err.message })
+        })
+})
 
-//OLD IMPLEMENTATION
-// router.post('/upload', multerModule.upload.single('testfile'), async (req, res, next) => {
-//     try {
-//         const tempFile = req.file;
-//         // console.log(req.file);
-//         // make sure file is available
-//         if (!tempFile) {
-//             res.status(400).json({
-//                 status: false,
-//                 data: 'No file provided.'
-//             });
-//         } else {
-//             res.json({
-//                 status: true,
-//                 message: 'File is uploaded.',
-//                 data: {
-//                     name: tempFile.originalname,
-//                     mimetype: tempFile.mimetype,
-//                     size: tempFile.size
-//                 }
-//             });
-//         }
-
-//     } catch (err) {
-//         console.log("Error!!!!", err);
-//         res.status(500).json({ err });
-//     }
-// });
 
 // router
 //   .route('/api/file/:id')
