7ab2f82f289746a8b18b463210d409543ff14af5
exia01 <gonzalezj@protonmail.com>
2020-07-09 22:24:02 -0400

    fix partial import errors

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-require('dotenv').config({path: __dirname + '/server/config/.env'}) //env file setup in config
-const express 				= require('express');
 const path 					= require('path');
 require('dotenv').config({ path: __dirname + '/server/config/.env' });
+const PORT 					= process.env.PORT || 3001;
+const express 				= require('express');
 // const favicon = require('express-favicon');
-const cors 					= require('cors');
+// const mongooseConnector = require('./config/mongoose')
+
+const authRoutes			= require('./server/routes/api/auth');
+const userRoutes			= require('./server/routes/api/users');
+const reportRoutes 			= require('./server/routes/reportRoutes');
+const generateRoutes		= require('./server/routes/generateRoutes')
+const accountRoutes 		= require('./server/routes/accountRoutes');
 const multer 				= require('multer');
 const morgan    			= require('morgan');
 const passport          	= require('passport');
 const fs 					= require('fs');
-const mongoose 				= require('mongoose');
-const mongooseConnector 	= require('./server/config/mongoose')
-const PORT 					= process.env.PORT || 8000;
+const jwt					= require('jsonwebtoken');
+const connectDB 			= require('./server/config/mongoose');
 
 
 
@@ -52,25 +57,8 @@ app.use(express.json({extended: false}));
 app.use(morgan('dev'));
 // app.use(morgan('combined'));
 
-//Multer Storage=
-// ('./server/utils/multerStorage.js')
-//Mongoose connection --> need import from config
-// mongooseConnector.mongooseConnection().then((res) => {
-//   console.log('Connected to DB \n');
-// })
-// .catch(err => {
-//   console.log('ERROR', err.message);
-// });
 connectDB();
 
-app.use('/', reportRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-
-// *** Temp Route for Testing data conversion *** //
-// app.use('/report', reportRoutes);
-app.use('/accounts', accountRoutes);
-app.use('/read', generateRoutes);
 
 app.use('/', reportRoutes);
 // *** Temp Route for Testing data conversion *** //
