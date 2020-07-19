7644fdf7f26259aec861726dfdfeb9dcdea37c5b
ehoversten <ehoversten@gmail.com>
2020-07-18 21:41:49 -0400

    filling out FileUpload component submit functionality

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -3,21 +3,59 @@ import axios from 'axios';
 
 const FileUpload = () => {
     //Setup State
+    const [file, setFile] = useState('');
+    const [fileName, setFileName] = useState('Choose File');
+    const [uploadedFile, setUploadedFile] = useState('');
+
+    const onChange = (evt) => {
+        setFile(evt.target.files[0]);
+        setFileName(evt.target.files[0].name);
+    }
+
+    const onSubmit = async (evt) => {
+        evt.preventDefault();
+        //-- Create a new form OBJ
+        const formData = new FormData();
+        //-- Add file to FORM OBJ
+        formData.append('file', file);
+
+        try {
+            const res = await axios.post('/upload/route', formData, {
+                headers : {
+                    'Content-Type':'multipart/form-data'
+                }
+            });
+
+            const { fileName, filePath } = res.data;
+            console.log("File Uploaded!!");
+            setUploadedFile({ fileName, filePath });
+        } catch(err) {
+
+        }
+    }
 
     return (
         <Fragment>
             <div className="container">
                 <h1>Upload CSV or Excel File:</h1>
-                <form action="#">
-                    <div class="file-field input-field">
-                        <div class="btn">
+                <form onSubmit={onSubmit}>
+                    <div className="file-field input-field">
+                        <div className="btn">
                             <span>Select File</span>
-                            <input type="file" />
+                            <input 
+                                type="file"
+                                name="file-select"
+                                onChange={onChange} />
                         </div>
-                        <div class="file-path-wrapper">
-                            <input class="file-path validate" type="text" />
+                        <div className="file-path-wrapper">
+                            <input 
+                                className="file-path validate" 
+                                type="text"
+                                name='file-path'
+                                onChange={onChange} />
                         </div>
                     </div>
+                    <button className="btn" type="submit">Upload</button>
                 </form>
             </div>
             <br/>
