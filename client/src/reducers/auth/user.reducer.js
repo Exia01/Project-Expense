79aa0297af7d35903862de791d51248d24daf8c0
J <Gonzalezj@protonmail.com>
2020-07-26 22:00:26 -0400

    Add user reducer and userActionType

---
+++ client/src/reducers/auth/user.reducer.js
@@ -0,0 +1,15 @@
+export const userReducer = (state, action) => {
+  //takes in action
+  switch (action.type) {
+    //could import the action types instead of typing the cases
+    case 'AUTH_USER':
+      const { token, isAuthenticated, user_obj } = action.user;
+      return { ...state, token, isAuthenticated, user: user_obj };
+    case 'LOGOUT':
+      return { isAuthenticated: false, user: null, token: null };
+    case 'REFRESH_TOKEN':
+      return { ...state, token: action.token };
+    default:
+      return state;
+  }
+};
---
+++ client/src/utils/constants/UserActionType.js
@@ -0,0 +1,3 @@
+export const AUTH_USER = 'AUTH_USER';
+export const LOGOUT = 'LOGOUT'
+// Originally under reducer...could put in reducer as constants.. match with Actions
