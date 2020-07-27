b65b6a750b95abcde6e32589012f01647cc15610
J <Gonzalezj@protonmail.com>
2020-07-26 22:01:07 -0400

    add userContext and localStorageReducer hook

---
+++ client/src/contexts/user.context.js
@@ -0,0 +1,32 @@
+import React, { createContext } from 'react';
+import { userReducer } from '../reducers/auth/user.reducer';
+import useLocalStorageReducer from '../hooks/useLocalStorageReducer';
+
+// separating contexts to prevent unnecessary rendering since user may changed when passed to a props
+export const UserContext = createContext();
+export const UserDispatchContext = createContext();
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null,
+};
+
+const UserContextProvider = (props) => {
+  //abstracted away for reusability could place back on file if needed
+  const [user, dispatch] = useLocalStorageReducer(
+    'user',
+    initialState, //default val
+    userReducer
+  );
+
+  return (
+    <UserContext.Provider value={user}>
+      <UserDispatchContext.Provider value={dispatch}>
+        {props.children}
+      </UserDispatchContext.Provider>
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
---
+++ client/src/hooks/useLocalStorageReducer.js
@@ -0,0 +1,40 @@
+import { useReducer, useEffect } from 'react';
+
+function useLocalStorageReducer(strKey, defaultValue, reducer) {
+  //receives the user reducer
+  //piece of state, based off of the value in localStorage using key
+  // console.log(strKey, defaultValue, reducer);
+
+  const [state, dispatch] = useReducer(reducer, defaultValue, () => {
+    // the third argument is for lazy loading purposes.
+    // the func will run and be set as the init state
+    // using a try - catch block the result of that will be stored as the value.
+    let value;
+    // used try-catch if application may accidentally set the user in localStorage to undefined
+    try {
+      value = JSON.parse(
+        window.localStorage.getItem(strKey) || String(defaultValue) //either grab by key or set by initial value
+      );
+    } catch (error) {
+      value = defaultValue || [];
+    }
+    return value;
+  });
+
+  //useEffect to update to localStorage when state changes with the reducer
+  useEffect(() => {
+    console.log('State:', state);
+    window.localStorage.setItem(strKey, JSON.stringify(state));
+    //when state changes we want to sync to local storage
+  }, [strKey, state]);
+
+  return [state, dispatch];
+
+  // console.log(state, strKey);
+}
+// We need to pass the key as a dependency because useEffect uses it in setItem and it does not know if it changes or not hence we need to pass it to make sure the old value is not used.
+
+export default useLocalStorageReducer;
+
+// more info: https://stackoverflow.com/questions/56425444/can-we-use-a-function-as-second-arguments-in-useeffect
+// used as such = const [user, setUser] = useLocalStorageState('user', [])
