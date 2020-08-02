584240714101aa2ada8f4e8414f2432fe3ab4037
J <Gonzalezj@protonmail.com>
2020-08-02 12:52:17 -0400

    update context, reducer. Create AuthModalBox

--- client/src/contexts/user.context.js
+++ client/src/contexts/user.context.js
@@ -8,11 +8,12 @@ export const UserDispatchContext = createContext();
 
 const initialState = {
   isAuthenticated: false,
-  user: null,
+  info: null,
   token: null,
   expiresAt: null,
 };
 
+//At some point maybe look into https cookies for token?
 const UserContextProvider = (props) => {
   //abstracted away for reusability could place back on file if needed
   const [user, dispatch] = useLocalStorageReducer(
@@ -21,8 +22,13 @@ const UserContextProvider = (props) => {
     userReducer
   );
 
+  const isAuthenticated = () => {
+    // return new Date().getTime() / 1000 < authState.expiresAt
+  
+  };
+
   return (
-    <UserContext.Provider value={user}>
+    <UserContext.Provider value={user, isAuthenticated}>
       <UserDispatchContext.Provider value={dispatch}>
         {props.children}
       </UserDispatchContext.Provider>
@@ -31,3 +37,8 @@ const UserContextProvider = (props) => {
 };
 
 export default UserContextProvider;
+
+
+//Check Authentication: https://www.codementor.io/@obabichev/react-token-auth-12os8txqo1
+
+// Info on JWT:https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#login
--- client/src/reducers/auth/user.reducer.js
+++ client/src/reducers/auth/user.reducer.js
@@ -4,12 +4,19 @@ export const userReducer = (state, action) => {
     //could import the action types instead of typing the cases
     case 'AUTH_USER':
       const { token, isAuthenticated, userInfo, expiresAt } = action.user;
-      return { ...state, token, isAuthenticated, user: userInfo, expiresAt };
+      return { ...state, token, isAuthenticated, info: userInfo, expiresAt };
     case 'LOGOUT':
-      return { isAuthenticated: false, user: null, token: null };
+      return {
+        isAuthenticated: false,
+        info: null,
+        token: null,
+        expiresAt: null,
+      };
     case 'REFRESH_TOKEN':
       return { ...state, token: action.token };
     default:
       return state;
   }
 };
+
+// Annonymous Funcs:https://www.javascripttutorial.net/javascript-anonymous-functions/
