4fa0d745b9e299c11e1f7b5b4618db280a6e050a
J <Gonzalezj@protonmail.com>
2020-08-03 21:47:18 -0400

    Add userContext to app

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import UserContextProvider from './contexts/user.context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = <UserContextProvider>
+    <App />
+</UserContextProvider>
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
