5c3c10b695f7556a4a12af3b97361bccc768a84d
ehoversten <ehoversten@gmail.com>
2020-08-18 21:41:15 -0400

    showing expenses saved in the db on ExpenseList Component

--- client/src/components/Expense/ExpenseList.js
+++ client/src/components/Expense/ExpenseList.js
@@ -4,12 +4,26 @@ function ExpenseList(props) {
 
     console.log(props);
     console.log(props.expenses);
-    
-    return(
-        <Fragment>
-            <h4>Expense List Component</h4>
-        </Fragment>
-    )
+
+    return (
+      <Fragment>
+        <h4>Current Expenses</h4>
+        {props.expenses.map(exp => (
+          <div class="row">
+            <div class="col s12 m6">
+              <div class="card blue-grey darken-1">
+                <div class="card-content white-text">
+                  <span class="card-title">{exp.title}</span>
+                  <p>
+                   {exp.description}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Fragment>
+    );
 }
 
 export default ExpenseList;
