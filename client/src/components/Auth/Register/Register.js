6062e76be782320dc1124a69e33eb4c7c79a6874
J <Gonzalezj@protonmail.com>
2020-08-30 14:33:55 -0400

    Update validation, add drawer

--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState, useContext } from 'react';
 import { Redirect } from 'react-router-dom';
-import axios from 'axios';
+import useErrorState from '../../../hooks/useErrorState';
 import {
   UserDispatchContext,
   UserContext,
@@ -18,6 +18,10 @@ import authAxiosInstance, {
 } from '../../../utils/axios/user.instance';
 
 function Register() {
+  const { isAuthenticated, user: authUserObj } = useContext(UserContext); //namedImport
+  const classes = useStyles();
+  const dispatch = useContext(UserDispatchContext);
+
   const [formData, setFormData] = useState({
     first: '',
     last: '',
@@ -26,28 +30,38 @@ function Register() {
     password: '',
     confirm: '',
   });
+  const [formErrors, setFormErrors] = useState({
+    first: '',
+    last: '',
+    username: '',
+    email: '',
+    password: '',
+    confirm: '',
+  });
+  const [resErrs, setResError] = useState({
+    serverError: '',
+    msgError: '',
+  });
   const [submittingForm, setSubmittingForm] = useState(false);
-
-  const classes = useStyles();
-  const { isAuthenticated, user: authUserObj } = useContext(UserContext); //namedImport
-  const dispatch = useContext(UserDispatchContext);
+  const [formIsValid, setFormIsValid] = useState(true);
   const { first, last, username, email, password, confirm } = formData;
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-  console.log(authUserObj);
+
+  // console.log(authUserObj);
   const onSubmit = async (e) => {
+    setFormErrors({
+      first: '',
+      last: '',
+      username: '',
+      email: '',
+      password: '',
+      confirm: '',
+    });
     e.preventDefault();
     setSubmittingForm(true);
-
-    if (password !== confirm) {
-      console.error('Passwords do not Match');
-    }
-
-    //-- TESTING --//
-    console.log('Submitting');
-    console.log(formData);
 
     //-- create temp user
     const user = {
@@ -58,52 +72,130 @@ function Register() {
       password,
     };
 
-    //-- Send to Server Route
-    try {
-      //-- Create 'config' for sending headers
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-          Authorization: `Bearer ${authUserObj.token}`,
-          'x-auth-token': authUserObj.token,
-        },
-      };
-      //-- Stringify User Input
-      const body = JSON.stringify(user);
-      console.log('********');
-      console.log(body);
+    validateForm(user);
 
-      // const res = await axios.post('/api/users/create', body, config);
-      const res = await authAxiosInstance.post('create', body, config);
-      //-- TESTING --//
-      console.log(res.data);
+    if (formIsValid) {
+      try {
+        //-- Create 'config' for sending headers
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${authUserObj.token}`,
+            'x-auth-token': authUserObj.token,
+          },
+        };
+        //-- Stringify User Input
+        const body = JSON.stringify(user);
 
-      //-- Clear inputs
-      setFormData({
-        first: '',
-        last: '',
-        username: '',
-        email: '',
-        password: '',
-        confirm: '',
-      });
+        const res = await authAxiosInstance.post('create', body, config);
 
-      const { token, userInfo, expiresAt } = res.data;
-      console.log(userInfo);
+        setFormData({
+          first: '',
+          last: '',
+          username: '',
+          email: '',
+          password: '',
+          confirm: '',
+        });
 
-      dispatch({
-        type: 'AUTH_USER',
-        user: { token, isAuthenticated: true, userInfo, expiresAt },
-      });
-      setSubmittingForm(false);
-    } catch (err) {
-      console.log(err);
-      setSubmittingForm(false);
-      // console.error(err.response.data);
-      // res.status(500).json(err);
+        const { token, userInfo, expiresAt } = res.data;
+
+        dispatch({
+          type: 'AUTH_USER',
+          user: { token, isAuthenticated: true, userInfo, expiresAt },
+        });
+        setSubmittingForm(false);
+      } catch (err) {
+        if (err.response) {
+          console.log(formErrors);
+          setResError({ ...resErrs, serverError: err.response.data.message });
+          // setFormErrors({
+          //   ...formErrors,
+          //   response: err.response.data.message,
+          // });
+        }
+        if (err.message) {
+          setResError({ ...resErrs, msgError: err.message });
+        }
+        // console.error(err.response.data);
+        setSubmittingForm(false);
+      }
     }
   };
+  function validateEmail(val) {
+    var pattern = new RegExp(
+      /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+    );
+    if (val == '') {
+      return 'Email is required';
+    }
+    if (!pattern.test(val)) {
+      // isValid = false;
+      return 'Please enter valid email address';
+
+      // return isValid
+    }
+    return '';
+  }
+
+  function validateForm({ ...user }) {
+    let tempErrorsFormObj = {
+      first: '',
+      last: '',
+      username: '',
+      email: '',
+      password: '',
+      confirm: '',
+    };
+    let tempIsValid = true;
+
+    if (typeof first !== 'undefined') {
+      let pattern = new RegExp(/^[a-zA-Z]+$/);
+      if (first == '') {
+        let firstError = 'Name is required';
+        tempErrorsFormObj.first = firstError;
+      } else {
+        if (!pattern.test(first)) {
+          tempIsValid = false;
+          let firstError = 'Only Letters';
+          tempErrorsFormObj.first = firstError;
+        }
+      }
+    }
+
+    tempErrorsFormObj.email = validateEmail(email);
+    if (typeof tempErrorsFormObj.email !== '') {
+      tempIsValid = false;
+    }
+
+    if (username == '') {
+      tempErrorsFormObj.username = 'Username is required';
+    }
+
+    if (password == '') {
+      let error = 'Please fill out password';
+      tempErrorsFormObj.password = error;
+      tempIsValid = false;
+    }
+    if (confirm == '') {
+      let error = 'Cannot be empty';
+      tempErrorsFormObj.confirm = error;
+      tempIsValid = false;
+    }
+    if (password !== confirm) {
+      let error = 'Passwords do not match';
+      tempErrorsFormObj.password = error;
+      tempErrorsFormObj.confirm = error;
+      tempIsValid = false;
+    }
+
+    if (!tempIsValid) {
+      setFormErrors(tempErrorsFormObj);
+      setFormIsValid(false);
+    }
+  }
+
   if (isAuthenticated()) {
     //-- Redirect to Landing
     return <Redirect to='/dashboard' />;
@@ -115,9 +207,15 @@ function Register() {
         <Row justify='center' align='middle'>
           <Col xs={12} sm={10} md={8} lg={6}>
             <h1 className='welcome'>Welcome, Register!</h1>
+
             <form className='register' onSubmit={(e) => onSubmit(e)}>
-              <Space direction='vertical' size='middle' className="w-100">
-                <div className={classes.formGroup}>
+              <Space direction='vertical' size='middle' className='w-100'>
+                <div
+                  className={
+                    (`${classes.formGroup}`,
+                    formErrors.first && 'ant-form-item-has-error')
+                  }
+                >
                   <label htmlFor='firstName' className={classes.formLabel}>
                     First Name
                   </label>
@@ -126,7 +224,13 @@ function Register() {
                     defaultValue={first}
                     onChange={handleChange}
                   />
+                  {formErrors.first && (
+                    <span className={classes.formError}>
+                      {formErrors.first}
+                    </span>
+                  )}
                 </div>
+
                 <div className={classes.formGroup}>
                   <label htmlFor='lastName' className={classes.formLabel}>
                     Last Name
@@ -137,7 +241,13 @@ function Register() {
                     onChange={handleChange}
                   />
                 </div>
-                <div className={classes.formGroup}>
+
+                <div
+                  className={
+                    (`${classes.formGroup}`,
+                    formErrors.username && 'ant-form-item-has-error')
+                  }
+                >
                   <label htmlFor='userName' className={classes.formLabel}>
                     Username
                   </label>
@@ -146,8 +256,19 @@ function Register() {
                     defaultValue={username}
                     onChange={handleChange}
                   />
+                  {formErrors.username && (
+                    <span className={classes.formError}>
+                      {formErrors.username}
+                    </span>
+                  )}
                 </div>
-                <div className={classes.formGroup}>
+
+                <div
+                  className={
+                    (`${classes.formGroup}`,
+                    formErrors.email && 'ant-form-item-has-error')
+                  }
+                >
                   <label htmlFor='email' className={classes.formLabel}>
                     Email
                   </label>
@@ -156,8 +277,19 @@ function Register() {
                     defaultValue={email}
                     onChange={handleChange}
                   />
+                  {formErrors.email && (
+                    <span className={classes.formError}>
+                      {formErrors.email}
+                    </span>
+                  )}
                 </div>
-                <div className={classes.formGroup}>
+
+                <div
+                  className={
+                    (`${classes.formGroup}`,
+                    formErrors.password && 'ant-form-item-has-error')
+                  }
+                >
                   <label htmlFor='password' className={classes.formLabel}>
                     Password
                   </label>
@@ -169,8 +301,19 @@ function Register() {
                       visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
                     }
                   />
+                  {formErrors.password && (
+                    <span className={classes.formError}>
+                      {formErrors.password}
+                    </span>
+                  )}
                 </div>
-                <div className={classes.formGroup}>
+
+                <div
+                  className={
+                    (`${classes.formGroup}`,
+                    formErrors.confirm && 'ant-form-item-has-error')
+                  }
+                >
                   <label
                     htmlFor='confirmPassword'
                     className={classes.formLabel}
@@ -185,6 +328,11 @@ function Register() {
                       visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
                     }
                   />
+                  {formErrors.confirm && (
+                    <span className={classes.formError}>
+                      {formErrors.confirm}
+                    </span>
+                  )}
                 </div>
                 <Button
                   type='primary'
--- client/src/components/Navbar.js/Navbar.js
+++ client/src/components/Navbar.js/Navbar.js
@@ -1,37 +1,64 @@
-import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
-import { Layout } from 'antd';
+import { Layout, Drawer, Button } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
+
 // CSS Styles And Components
-import '../../styles/NavbarStyles.css';
-
+// import '../../styles/NavbarStyles.css';
 import useStyles from '../../styles/NavbarStyles';
 import { useTheme } from 'react-jss';
 import { ThemeContext } from '../../contexts/theme.context';
-const { Content } = Layout;
 
 export default function Navbar(props) {
   // / Creating a namespaced theming object.
   // const theme = useTheme()
   // const classes = useStyles({ ...props, theme })
-  const themeProps = useContext(ThemeContext)
-  const classes = useStyles(themeProps)
+  const themeProps = useContext(ThemeContext);
+  const classes = useStyles(themeProps);
+  const [isDrawerVisible, setIsDrawerVisible] = useState(false);
+  const showDrawer = () => {
+    setIsDrawerVisible(true);
+  };
+  const hideDrawer = () => {
+    setIsDrawerVisible(false);
+  };
   return (
     <header className={classes.root}>
-      <div className={`container-fluid navbar`}>
-        <div className='logo'></div>
-        <ul className='navbar-menu'>
+      <Drawer
+        // title='Basic Drawer'
+        placement='right'
+        closable={false}
+        onClose={hideDrawer}
+        visible={isDrawerVisible}
+      >
+        <p onClick={hideDrawer}>
+          <Link to='/register'>Register</Link>
+        </p>
+
+        <p onClick={hideDrawer}>
+          <Link to='/login'>Login</Link>
+        </p>
+      </Drawer>
+      <div className={`container-fluid ${classes.navbar}`}>
+        <div className={classes.logo}>
+          <Link to='/'>Home</Link>
+        </div>
+        <ul className={classes.navBarMenu}>
           <li>
-            <NavLink exact to='/register' className={`test`}>
+            <NavLink exact to='/register' className={'navlink'}>
               register
             </NavLink>
           </li>
           <li>
-            <NavLink exact to='/login' className={`test`}>
+            <NavLink exact to='/login' className={`navlink`}>
               Login
             </NavLink>
           </li>
         </ul>
+        <div className={classes.hamburgerMenu}>
+          <MenuOutlined onClick={showDrawer} />
+        </div>
       </div>
     </header>
   );
